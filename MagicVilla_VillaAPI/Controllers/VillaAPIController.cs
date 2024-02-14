58d736f04e4b6b87df3d437e53c95db95aa108fa
Tigran <tmanukian91@gmail.com>
2024-02-14 18:34:47 +0400

    Added VillaDTO and added GetVillas controller method

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,22 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+	[Route("api/VillaAPI")]
+	[ApiController]
+	public class VillaAPIController:ControllerBase
+	{
+
+		[HttpGet]
+		public IEnumerable<VillaDTO> GetVillas()
+		{
+			return new List<VillaDTO>
+			{
+				new() {Id=1, Name = "Pool View" },
+				new() {Id=2, Name = "Beach View"}
+			};
+		}
+	}
+}
